2e89fe24b62592ee5c0336a5b2f28e944dce1239
SORA-OB <arturoortiz2229@gmail.com>
2025-09-27 18:54:30 -0500

    uwu

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+        './index.html',
+        './src/**/*.{js,jsx,ts,tsx}',
+    ],
+    theme: {
+        extend: {
+            colors: {
+            },
+            fontFamily: {
+                sans: ['Poppins', 'ui-sans-serif', 'system-ui'],
+            },
+        },
+    },
+    plugins: [],
+};
